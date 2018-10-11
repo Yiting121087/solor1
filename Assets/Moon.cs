7587a94c69b47c16884f86e9a2da845790ac7a0c
YITing <doris121087@gmail.com>
2018-10-11 11:48:09 +0800

    Initial commit

---
+++ Assets/Moon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour{
+
+    public float y;
+
+    void Update()
+    {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y * Time.deltaTime, 0);
+        transform.Rotate(angle);
+    }
+}
